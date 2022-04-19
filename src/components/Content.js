b57f279205b6c582ffda22c29006911049d9b6c6
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-04-19 17:02:52 +0200

    added background image and github button

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,21 +7,39 @@ const Content = () => {
   return (
     <div className='wrapper'>
 
-<section class="hero is-large header-image">
-  <div class="hero-body">
-    <p class="title is-white">
+  <section className="hero is-fullheight header-image">
+  <nav className="navbar">
+  <div className="container">
+    <div id="navMenu" className="navbar-menu">
+
+      </div>
+
+      <div className="navbar-end">
+        <div className="navbar-item">
+          <div className="buttons">
+            <a href='https://github.com/codingholt/CoinGecko-CLI' className="button is-dark">Github</a>
+          </div>
+        </div>
+      </div>
+    </div>
+</nav>
+  <div className="hero-body">
+  <div className="">
+    <p className="title has-text-white">
     CoinGecko-Cli, a new way to get data from coingecko
     </p>
-    <p class="subtitle">
+    <p className="subtitle has-text-white">
     To get started run the commmand:
     </p>
-    <div className='column is-half is-offset-6'>
-    <ReactTerminalCommand command='npm install -g coingecko-cli' className='i is-pulled-right'/>
-    </div>
   </div>
-   
-</section>
+    <div className='column is-offset-2'>
+    <ReactTerminalCommand command='npm install -g coingecko-cli' className='i is-pulled-right'/>
 </div>
+    </div>
+    </section>
+  </div>
+
+
 )
 }
 
